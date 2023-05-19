28b6639a9c592b77e7e159fcfd15bbe1295f44cb
Nchor Michael <Manchor127@gmail.com>
2023-05-19 12:45:55 +0000

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -120,13 +120,15 @@ const technologies = [
 
 const experiences = [
   {
-    title: "React.js Developer",
-    company_name: "Starbucks",
+    title: "Software Developer",
+    company_name: "University Information Technology Services (UITS)",
     icon: starbucks,
     iconBg: "#383E56",
-    date: "March 2020 - April 2021",
+    date: "Oct 2022 - Oct 2023",
     points: [
-      "Developing and maintaining web applications using React.js and other related technologies.",
+      "Built a C# ASP.NET and SQL Server, Tailwindcss, ExpressJS, NodeJS and EJS Templating application to digitise student hard ID cards into soft cards which can be scanned to verify student status.",
+      "Testing and Data Entry for a .NET Blazortimetable application to help departments in KNUST schedule classes and meetings efficiently.",
+      "Development of a Shopping Cart Application with C# ASP.NET.",
       "Collaborating with cross-functional teams including designers, product managers, and other developers to create high-quality products.",
       "Implementing responsive design and ensuring cross-browser compatibility.",
       "Participating in code reviews and providing constructive feedback to other developers.",
