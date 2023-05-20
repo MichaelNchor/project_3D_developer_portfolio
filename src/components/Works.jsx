d5ac572c70846c5dbe1f28743fdd0c468df5d1ce
Nchor Michael <Manchor127@gmail.com>
2023-05-20 20:12:52 +0000

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -62,6 +62,7 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
+        <button type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Purple</button>
       </Tilt>
 //     </motion.div>
   );
