c71b67f7640af2000bdd40ecb14145cfca9dc5b1
Nchor Michael <Manchor127@gmail.com>
2023-05-20 20:29:05 +0000

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -63,8 +63,8 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
+        <button  onClick={() => window.open(live_demo_link, "_blank")} type="button" className="m-3 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Live Demo</button>
       </Tilt>
-      <button  onClick={() => window.open(live_demo_link, "_blank")} type="button" className="focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Live Demo</button>
 //     </motion.div>
   );
 };
