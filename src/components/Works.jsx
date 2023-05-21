f12d7424a747c5faadd7fe118bb9bcd708403d95
Nchor Michael <Manchor127@gmail.com>
2023-05-21 08:21:21 +0000

    Update Works.jsx

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -63,7 +63,7 @@ const ProjectCard = ({
             </p>
           ))}
         </div>
-        <button  onClick={() => window.open(live_demo_link, "_blank")} type="button" className="my-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Live Demo</button>
+        <button  onClick={() => window.open(live_demo_link, "_blank")} type="button" className="my-3 focus:outline-none text-white bg-purple-700 hover:bg-purple-800 focus:ring-4 focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 mb-2 dark:bg-purple-600 dark:hover:bg-purple-700 dark:focus:ring-purple-900">Live Demo</button>
       </Tilt>
 //     </motion.div>
   );
